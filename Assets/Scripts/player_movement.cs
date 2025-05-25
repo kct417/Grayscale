c19810a508d802bac9d00a264a8cfe6ae62520a2
nnguy239 <nnguy239cp@gmail.com>
2025-05-24 23:34:23 -0700

    player moving

---
+++ Assets/Scripts/player_movement.cs
@@ -0,0 +1,27 @@
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class player_movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+}
