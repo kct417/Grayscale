7985509d44c01e0f7cf800be60561e1c7b8e22e1
nnguy239 <nnguy239cp@gmail.com>
2025-05-26 18:53:26 -0700

    player animatinons

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -1,4 +1,3 @@
-using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,17 +6,51 @@ public class player_movement : MonoBehaviour
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator animator;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         rb.linearVelocity = moveInput * moveSpeed;
+
+        if (moveInput != Vector2.zero)
+        {
+            animator.SetBool("isMoving", true);
+
+            // Determine dominant direction
+            if (Mathf.Abs(moveInput.x) > Mathf.Abs(moveInput.y))
+            {
+                // Horizontal movement
+                float moveX = moveInput.x > 0 ? 1f : -1f;
+                animator.SetFloat("MoveX", moveX);
+                animator.SetFloat("MoveY", 0f);
+                Debug.Log("Set MoveX: " + moveX + ", MoveY: 0");
+
+                // 🔁 Flip sprite based on horizontal direction
+                if (moveInput.x > 0)
+                    transform.localScale = new Vector3(-1f, 1f, 1f);  // Facing right
+                else
+                    transform.localScale = new Vector3(1f, 1f, 1f);   // Facing left
+            }
+            else
+            {
+                // Vertical movement
+                float moveY = moveInput.y > 0 ? 1f : -1f;
+                animator.SetFloat("MoveX", 0f);
+                animator.SetFloat("MoveY", moveY);
+                Debug.Log("Set MoveX: 0, MoveY: " + moveY);
+            }
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+            Debug.Log("Player is idle");
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
